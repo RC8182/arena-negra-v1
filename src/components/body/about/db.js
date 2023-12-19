3d68809da64eb72b9d6e975023f58cabcde6b442
RC8182 <javiervisconti@hotmail.com>
2023-12-19 10:28:11 +0000

    about us changed

--- src/components/body/about/db.js
+++ src/components/body/about/db.js
@@ -4,7 +4,7 @@ export const datos=
 
         about:{
             titulo:'Sobre Nosotros',
-            texto:'Somos una familia apasionada por el mundo de la restauración. En Arena Negra Restaurant estamos comprometidos en brindar calidad y servicio a nuestros clientes. Detrás de esta familia contamos con un maravilloso equipo humano entre los que se encuentran: Tomás, Miguel, Dino, Claudia y Javier en sala. Y en cocina: Felix, Yuri y nuestro Chef internacional Aday Martín. Un equipo inmejorable en el que cada uno aporta su experiencia para que la gestión de cocina, servicio, compras y elaboración del producto se encuentre siempre a la altura del cliente.'
+            texto:' Somos una familia apasionada por el mundo de la restauración. En Arena Negra Restaurant, estamos comprometidos en brindar calidad y servicio a nuestros clientes. Detrás de esta familia, contamos con un maravilloso equipo humano que se encarga de la sala y la cocina. Cada miembro de nuestro equipo aporta su experiencia y habilidades únicas, asegurando que la gestión de la cocina, el servicio, las compras y la elaboración del producto siempre estén a la altura de las expectativas de nuestros clientes. Juntos, trabajamos incansablemente para crear una experiencia culinaria inolvidable en cada visita.'
            },
 },
 
@@ -12,7 +12,7 @@ export const datos=
 
         about:{
             titulo:'About Us',
-            texto:"We are a family passionate about the world of restoration. At Arena Negra Restaurant we are committed to providing quality and service to our customers. Behind this family we have a wonderful human team among which are: Tomás, Miguel, Dino, Claudia and Javier in the room.  And in the kitchen: Felix, Yuri and our international Chef Aday Martín.An unbeatable team in which each one contributes their experience so that the management of the kitchen, service, purchases and preparation of the product is always at customer level."
+            texto:"We are a family passionate about the world of restoration. At Arena Negra Restaurant, we are committed to providing quality and service to our customers. Behind this family, we have a wonderful human team that takes care of the dining room and the kitchen. Each member of our team brings their unique experience and skills, ensuring that the management of the kitchen, service, purchases, and product preparation always meet our customers’ expectations. Together, we work tirelessly to create an unforgettable culinary experience with each visit."
             },    
 
     }
