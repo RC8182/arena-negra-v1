9051171caaff413d2b8a48dbf9e532e091741bd8
RC8182 <javiervisconti@hotmail.com>
2024-01-12 16:05:37 +0000

    layout cleaned

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,11 @@
 import './globals.css'
-import Footer from '@/components/footer/footer'
 import { ArenaProvider } from '@/context/arenaProvider'
-import NavBar from '@/components/navbar/navbar'
 import { Providers } from './providers'
-import  FootBar  from '@/components/footer/footBar'
-import { HStack } from '@chakra-ui/react'
 
 
 
 export const metadata = {
-  title: 'Arena Negra Restaurante - Los Abrigos, Tenerife',
+  title: 'Restaurante en Los Abrigos - Arena Negra',
   description: 'Disfruta de las mejores paellas y parrilladas de pescado local en Los Abrigos, Tenerife. Arena Negra Restaurant ofrece una experiencia única con vistas impresionantes y un servicio al cliente inmejorable.',
   keywords: 'Restaurante, Paella, Mariscos, Pescado Local, Los Abrigos, Tenerife, Vistas al Mar, Servicio Excelente, Restaurante en los Abrigos, Donde comer pescado en los abrigos, donde comer paella en los abrigos'
 }
@@ -18,16 +14,15 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang='es'>
-      <meta name="google-site-verification" content="Rm2sDQs-MVKtHT9LyDkRsSFVTAVbA0OHFvWdRa7I3F8" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="google-site-verification" content="Rm2sDQs-MVKtHT9LyDkRsSFVTAVbA0OHFvWdRa7I3F8" />
+        <meta name="theme-color" content="black"></meta>
       <body>
         <Providers>
           <ArenaProvider>
-            <NavBar/>
-          {children}
-          <Footer/>
-          <HStack display={{ base: 'flex', md: 'none' }}>
-          <FootBar/>
-          </HStack>
+            {children}
           </ArenaProvider>
         </Providers>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
 import { Body } from "@/components/body/body";
+import FootBar from "@/components/footer/footBar";
+import Footer from "@/components/footer/footer";
+import NavBar from "@/components/navbar/navbar";
+import { HStack } from "@chakra-ui/react";
 
 export default function App(){
     return(
         <div>
-            <Body/>
+        <NavBar/>
+        <Body/>
+        <Footer/>
+        <HStack display={{ base: 'flex', md: 'none' }}>
+          <FootBar/>
+        </HStack>
         </div>
     )
 }
